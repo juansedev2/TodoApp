c911906e212a68d460f2cdfdefcb5e86a6553c78
juansedev2 <jsab62tol@gmail.com>
2024-05-05 21:25:47 -0500

    Fix the bootstrap file to include the right way to invoke the files

--- src/core/Bootstrap.php
+++ src/core/Bootstrap.php
@@ -1,16 +1,10 @@
 <?php
-$APP_PATH = $_SERVER["DOCUMENT_ROOT"];
-require_once $APP_PATH . "/core/functions/Functions.php";
-require $APP_PATH . "/core/Injector.php";
-require $APP_PATH . "/core/database/DBConnection.php";
-require $APP_PATH . "/core/database/QueryBuilder.php";
-// Models of the app
-require $APP_PATH . "/app/models/Model.php";
-require $APP_PATH . "/app/models/Serie.php";
-// Router and routes
-require $APP_PATH . "/core/router/Router.php";
-require $APP_PATH . "/core/router/Handler.php";
+// ! This files init the important configurations of the app
+use Jdev2\TodoApp\core\Injector;
+use Jdev2\TodoApp\core\database\DBConnection;
+use Jdev2\TodoApp\core\database\QueryBuilder;
 
+$APP_PATH = $_SERVER["DOCUMENT_ROOT"];
 Injector::set($APP_PATH, "app-route");
 Injector::set(require __DIR__ . "/config/Config.php", "config");
 
