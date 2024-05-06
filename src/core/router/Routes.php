f7596f23aae4e8044d5cc8a0edc94b7ce45808c1
juansedev2 <jsab62tol@gmail.com>
2024-05-05 21:24:35 -0500

    updated the routes of the app in arrays to fix the router

--- src/core/router/Routes.php
+++ src/core/router/Routes.php
@@ -1,11 +1,7 @@
 <?php
 //This file contains the routes of the app
 return [
-    "" => "controllers/GetSeries.php",
-    "/" => "controllers/GetSeries.php",
-    "index" => "controllers/GetSeries.php",
-    "updateSerieForm" => "controllers/UpdateSerieForm.php",
-    "updateSerie" => "controllers/UpdateSerie.php",
-    "createSerie" => "controllers/CreateSerie.php",
-    "deleteSerie" => "controllers/DeleteSerie.php"
+    "" => ["HomeController", "showLanding"],
+    "/" => ["HomeController", "showLanding"],
+    "login" => ["LoginController", "loginUser"]
 ];
