96e4b76a0cf32af138a599c9dd8f7ab30afc2be9
juansedev2 <jsab62tol@gmail.com>
2024-05-07 17:07:25 -0500

    Add the initial files of the register view

---
+++ public/views/Register.view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+    <?php include_once "./public/partials/GeneralHead.partial.php"?>
+    <link href="/public/css/Register.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+      <?php include_once "./public/partials/NavBar.partial.php" ?>
+    </header>
+    <main>
+        <div class="container-sm mw-300 container-form border border-black border-opacity-25 rounded p-5">
+          <form method="post" action="login">
+          </form>
+        </div>
+    </main>
+    <?php include_once "./public/partials/BootstrapJS.partial.php"?>
+</body>
+</html>
