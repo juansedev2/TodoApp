69ae416478342e906079ce7e1a8eedc8174180a2
juansedev2 <jsab62tol@gmail.com>
2024-05-07 16:57:58 -0500

    Update the name of the db to connect

--- src/core/config/Config.php
+++ src/core/config/Config.php
@@ -5,7 +5,7 @@ return [
         "sgbd" => "mysql",
         "host" => "127.0.0.1",
         "port" => "3306",
-        "name" => "animesappdb",
+        "name" => "todoappdatabase",
         "user" => "root",
         "password" => "ubddssa2"
     ],
