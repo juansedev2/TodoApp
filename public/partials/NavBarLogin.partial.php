a3bf4f55091391fb807849d33112cee5e0b42449
juansedev2 <jsab62tol@gmail.com>
2024-05-09 21:11:43 -0500

    Add the partial of the navbar of the logued user

---
+++ public/partials/NavBarLogin.partial.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary mb-5">
+    <div class="container-fluid flex-row-reverse">
+        <h1>TodoApp</h1>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="/">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/acerca/">Acerca de</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/cerrar-sesion/">CERRAR SESIÓN</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
