71a826352fb1aa22d75a066a61ee892742eb946f
juansedev2 <jsab62tol@gmail.com>
2024-05-14 17:33:18 -0500

    Add the vlidations on the crud operations of the TaskController in the inputs

--- src/app/controllers/TaskController.php
+++ src/app/controllers/TaskController.php
@@ -2,10 +2,12 @@
 namespace Jdev2\TodoApp\app\controllers;
 
 use Exception;
+use Jdev2\TodoApp\app\models\Task;
+use Jdev2\TodoApp\core\helpers\InputValidator;
 use Jdev2\TodoApp\core\helpers\SessionValidator;
 use Jdev2\TodoApp\app\controllers\BaseController;
 use Jdev2\TodoApp\app\controllers\LoginController;
-use Jdev2\TodoApp\app\models\Task;
+use Jdev2\TodoApp\app\controllers\ExceptionController;
 
 
 class TaskController extends BaseController{
@@ -13,15 +15,19 @@ class TaskController extends BaseController{
     public function showUpdateTaskView(){
         if(static::validateSession()){
             $id_task = $_POST["id_task"] ?? "";
+            $id_task = InputValidator::escapeCharacters(InputValidator::cleanWhiteSpaces($id_task));
+
             if(empty($id_task)){
-                return throw new Exception("NINGUN DATO FUE ENVIADO, MOSTRAR ERROR", 1);
+                //return throw new Exception("NINGUN DATO FUE ENVIADO, MOSTRAR ERROR", 1);
+                return (new LoginController)->showWelcome(["message" => "Error de solicitud", "color" => "alert-warning"]);
             }else{
                 $task = new Task;
                 $result = $task->getInfoTaskById($id_task);
                 if($result){
                     return static::returnView("UpdateTask", ["task" => $task]);
                 }else{
-                    return throw new Exception("404, tarea no encontrada", 1);
+                    //return throw new Exception("404, tarea no encontrada", 1);
+                    return ExceptionController::returnNotFoundView();
                 }
             }
         }else{
@@ -36,15 +42,28 @@ class TaskController extends BaseController{
             $id_task = $_POST["task-id"] ?? "";
             $title_task = $_POST["task-title"] ?? "";
             $task_description = $_POST["task-description"] ?? "";
+
+            $id_task = InputValidator::escapeCharacters(InputValidator::cleanWhiteSpaces($id_task));
+            $title_task = InputValidator::escapeCharacters($title_task);
+            $task_description = InputValidator::escapeCharacters($task_description);
+
+            if(!InputValidator::validateMaxLenght($title_task, 30)){
+                return (new LoginController)->showWelcome(["message" => "El título no puede superar los 30 caracteres", "color" => "alert-warning"]);
+            }
+            if(!InputValidator::validateMaxLenght($task_description, 200)){
+                return (new LoginController)->showWelcome(["message" => "La descripción de la tarea no puede superar los 200 caracteres", "color" => "alert-warning"]);
+            }
             
-            if(empty($id_task) || empty($title_task) || empty($task_description)){
-                return throw new Exception("DATOS INCOMPLETOS MOSTRAR ERROR", 1);
+            if(!InputValidator::validateAllEmptyStrings([$id_task, $title_task, $task_description])){
+                // return throw new Exception("DATOS INCOMPLETOS MOSTRAR ERROR", 1);
+                return (new LoginController)->showWelcome(["message" => "Error, datos incompletos", "color" => "alert-warning"]);
             }else{
                 $result = (new Task)->updateTask($id_task, $title_task, $task_description);
                 if($result){
                     return static::redirectTo("welcome");
                 }else{
-                    return throw new Exception("500, no se pudo actualizar la tarea, error de servidor", 1);
+                    //return throw new Exception("500, no se pudo actualizar la tarea, error de servidor", 1);
+                    return (new LoginController)->showWelcome(["message" => "Hubo un error al intentar insertar la tarea, por favor intentar después", "color" => "alert-danger"]);
                 }
             }
         }else{
@@ -58,15 +77,29 @@ class TaskController extends BaseController{
             $id_user = SessionValidator::returnIdentificator();
             $title_task = $_POST["title-task"] ?? "";
             $task_description = $_POST["description-task"] ?? "";
+
+            $id_user = InputValidator::escapeCharacters(InputValidator::cleanWhiteSpaces($id_user));
+            $title_task = InputValidator::escapeCharacters($title_task);
+            $task_description = InputValidator::escapeCharacters($task_description);
+
+            if(!InputValidator::validateMaxLenght($title_task, 30)){
+                return (new LoginController)->showWelcome(["message" => "El título no puede superar los 30 caracteres", "color" => "alert-warning"]);
+            }
+            if(!InputValidator::validateMaxLenght($task_description, 200)){
+                return (new LoginController)->showWelcome(["message" => "La descripción de la tarea no puede superar los 200 caracteres", "color" => "alert-warning"]);
+            }
             
-            if(empty($id_user) || empty($title_task) || empty($task_description)){
-                return throw new Exception("DATOS INCOMPLETOS MOSTRAR ERROR", 1);
+            if(!InputValidator::validateAllEmptyStrings([$id_user, $title_task, $task_description])){
+                return (new LoginController)->showWelcome(["message" => "Error, datos incompletos", "color" => "alert-warning"]);
             }else{
+
                 $result = (new Task)->insertTask($id_user, $title_task, $task_description);
+
                 if($result){
                     return static::redirectTo("welcome");
                 }else{
-                    return throw new Exception("500, no se pudo crear la tarea, error de servidor", 1);
+                    //return throw new Exception("500, no se pudo crear la tarea, error de servidor", 1);
+                    return (new LoginController)->showWelcome(["message" => "Hubo un error al intentar insertar la tarea, por favor intentar después", "color" => "alert-danger"]);
                 }
             }
         }else{
@@ -79,15 +112,17 @@ class TaskController extends BaseController{
         if(static::validateSession()){
             
             $id_task_input_button = $_POST["id_task_input_button"] ?? "";
+            $id_task_input_button = InputValidator::escapeCharacters(InputValidator::cleanWhiteSpaces($id_task_input_button));
             
             if(empty($id_task_input_button)){
-                return throw new Exception("DATOS INCOMPLETOS MOSTRAR ERROR", 1);
+                return (new LoginController)->showWelcome(["message" => "Error de solicitud", "color" => "alert-danger"]);
             }else{
                 $result = (new Task)->deleteTask($id_task_input_button);
                 if($result){
                     return static::redirectTo("welcome");
                 }else{
-                    return throw new Exception("500, no se pudo eliminar la tarea, error de servidor", 1);
+                    //return throw new Exception("500, no se pudo eliminar la tarea, error de servidor", 1);
+                    return ExceptionController::returnInternalServerErrorView();
                 }
             }
         }else{
