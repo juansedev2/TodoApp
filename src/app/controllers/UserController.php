8f5b743a24c1e5448d9f4f7e388f88d2169b81f2
juansedev2 <jsab62tol@gmail.com>
2024-05-14 17:34:03 -0500

    Add the input validations and the show user view erros instedthrow exceptions

--- src/app/controllers/UserController.php
+++ src/app/controllers/UserController.php
@@ -6,27 +6,27 @@ use Jdev2\TodoApp\app\models\User;
 use Jdev2\TodoApp\app\controllers\BaseController;
 use Jdev2\TodoApp\core\helpers\InputValidator;
 use Jdev2\TodoApp\core\security\Encryptor;
+use Jdev2\TodoApp\app\controllers\ExceptionController;
 
 class UserController extends BaseController{
 
     /**
      * This function gets the welcome view for the user, also show the tasks if the user have
     */
-    public function returnWelcomeView(User $user = null, string | int $id_user = null){
+    public function returnWelcomeView(User $user = null, string | int $id_user = null, array $alert_message = []){
         if(is_null($user) && is_null($id_user)){
-            return throw new Exception("El parametro user o id_user NO PUEDEN SER NULOS, DEBE ENVIARSE AL MENOS 1", 1);
+            // return throw new Exception("El parametro user o id_user NO PUEDEN SER NULOS, DEBE ENVIARSE AL MENOS 1", 1);
+            return ExceptionController::returnInternalServerErrorView();
         }else if($user){
             $user->getTasksForUser($user->id_user);
         }else{
             $user = new User();
             $user->getTasksForUser($id_user);
         }
-        //dd($user->tasks[0]);
-        static::returnView("UserMain", ["user" => $user]);
+        static::returnView("UserMain", array_merge(["user" => $user], $alert_message));
     }
 
     public function createUser(){
-        // TODO: TERMINAR LA VALIDACIÓN DE LOS CAMPOS QUE LLEGAN
         
         $name = $_POST["name"] ?? "";
         $last_name = $_POST["last-name"] ?? "";
@@ -34,9 +34,42 @@ class UserController extends BaseController{
         $password = $_POST["password"] ?? "";
         $password2 = $_POST["password2"] ?? "";
 
+        $name = InputValidator::escapeCharacters($name);
+        $last_name = InputValidator::escapeCharacters($last_name);
+        $email = InputValidator::escapeCharacters($email);
+        $password = InputValidator::escapeCharacters($password);
+        $password2 = InputValidator::escapeCharacters($password2);
+
         if(InputValidator::validateAllEmptyStrings([$name, $last_name, $email, $password, $password2])){
+
+            if(!InputValidator::validateMaxLenght($name, 20)){
+                return static::returnView("Register", ["alert" => "El nombre del usuario no puede superar los 20 caracteres", "color_alert" => "alert-warning"]);
+            }
+
+            if(!InputValidator::validateMaxLenght($last_name, 20)){
+                return static::returnView("Register", ["alert" => "El apellido del usuario no puede superar los 20 caracteres", "color_alert" => "alert-warning"]);
+            }
+
+            if(!InputValidator::validateIfIsEmail($email)){
+                return static::returnView("Register", ["alert" => "Formato de correo no válido", "color_alert" => "alert-warning"]);
+            }
+
+            if(!InputValidator::validateIfPasswordIsSecure($password)){
+                return static::returnView("Register", ["alert" => "La contraseña debe ser de mínimo 8 caracteres, al menos una letra en maýuscula, al menos un número y un caracter especial, al menos", "color_alert" => "alert-warning"]);
+            }
+
+            if(!InputValidator::validateIfPasswordIsSecure($password2)){
+                return static::returnView("Register", ["alert" => "La contraseña debe ser de mínimo 8 caracteres, al menos una letra en maýuscula, al menos un número y un caracter especial, al menos", "color_alert" => "alert-warning"]);
+            }
+
+            if($password !== $password2){
+                return static::returnView("Register", ["alert" => "Las contraseñas no coinciden", "color_alert" => "alert-warning"]);
+            }
+
             $password = Encryptor::encryptPassword($password);
+
             $result = User::create(["name" => $name, "last_name" => $last_name, "email" => $email, "password" => $password]);
+
             if($result->state_creation_operation){
                 return static::returnView("Register", ["alert" => "Usuario registado exitosamente, ya puedes iniciar sesión con tus datos", "color_alert" => "alert-success"]);
             }else{
