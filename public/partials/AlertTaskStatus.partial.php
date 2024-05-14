21fa2bc887a15b87a41a3f4e658f1b03cb4c3175
juansedev2 <jsab62tol@gmail.com>
2024-05-14 17:19:57 -0500

    Add the section to alert messages into the main user view

---
+++ public/partials/AlertTaskStatus.partial.php
@@ -0,0 +1,5 @@
+<div class="container text-center">
+    <?php if(!(empty($message) && (empty($color)))):?>
+        <div class="alert <?=$color?>" role="alert"><?=$message?></div>
+    <?php endif;?>
+</div>
