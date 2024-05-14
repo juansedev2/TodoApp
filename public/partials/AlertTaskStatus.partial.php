8ca18aa0c008cfee4e5de9cd78e93f7cd9b9c066
juansedev2 <jsab62tol@gmail.com>
2024-05-14 17:30:48 -0500

    Updatehe the AlertTaskStatus partial

--- public/partials/AlertTaskStatus.partial.php
+++ public/partials/AlertTaskStatus.partial.php
@@ -1,5 +1,5 @@
 <div class="container text-center">
-    <?php if(!(empty($message) && (empty($color)))):?>
+    <?php if(!(empty($message) || (empty($color)))):?>
         <div class="alert <?=$color?>" role="alert"><?=$message?></div>
     <?php endif;?>
 </div>
