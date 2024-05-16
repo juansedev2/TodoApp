31a354585cf21dd010665f2b38ed1ba108a63ea8
juansedev2 <jsab62tol@gmail.com>
2024-05-15 21:01:16 -0500

    Add the forbiden view

---
+++ public/views/Forbidden.view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>No autorizado</title>
+    <?php include_once "./public/partials/GeneralHead.partial.php"?>
+</head>
+<body>
+    <section class="py-3 py-md-5 min-vh-100 d-flex justify-content-center align-items-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="text-center">
+                        <h1 class="d-flex justify-content-center align-items-center gap-2 mb-4">403 NO PERMITIDO</h1>
+                        <h3 class="h2 mb-2">Oops! La acción que quieres realizar no es permitida para tí.</h3>
+                        <p class="mb-5">Lo sentimos, cualquier novedad por favor comunicarse con la admisnitración, por favor revisar la sección de Acerca de.</p>
+                        <a class="btn bsb-btn-5xl btn-dark rounded-pill px-5 fs-6 m-0" href="/inicio/" role="button">Regresar al inicio</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
