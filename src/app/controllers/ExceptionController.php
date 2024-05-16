b7b90a77157d341a36e866dc2aa4bfa5f3744228
juansedev2 <jsab62tol@gmail.com>
2024-05-15 21:07:21 -0500

    Modify the functions of shows errors views, this function should ending with the process;

--- src/app/controllers/ExceptionController.php
+++ src/app/controllers/ExceptionController.php
@@ -11,16 +11,27 @@ class ExceptionController extends BaseController{
      * This function return the 404 view not found view
     */
     public static function returnNotFoundView(){
-        header("HTTP/1.0 404 Not Found", true, 404);
-        return static::returnView("NotFound");
+        header("HTTP/1.1 404 Not Found", true, 404);
+        static::returnView("NotFound");
+        exit();
     }
 
     /**
      * This function return the 500 internal server error view
     */
     public static function returnInternalServerErrorView(){
-        header("HTTP/1.0 500 Internal Server Error", true, 500);
-        return static::returnView("InternalServerError");
+        header("HTTP/1.1 500 Internal Server Error", true, 500);
+        static::returnView("InternalServerError");
+        exit();
+    }
+
+    /**
+     * This function return the 500 internal server error view
+    */
+    public static function returnForbidden(){
+        header("HTTP/1.1 403 Forbidden", true, 403);
+        static::returnView("Forbidden");
+        exit();
     }
 
 }
