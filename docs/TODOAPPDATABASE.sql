0a667107c84300d968f1bae8e4b412882b54cc46
Jdev2 <jsab62tol@gmail.com>
2024-05-20 15:42:10 -0500

    Updated the final version of the full sql script of the todoapp database

--- docs/TODOAPPDATABASE.sql
+++ docs/TODOAPPDATABASE.sql
@@ -1,42 +1,267 @@
--- Create the database and use it
-create database todoappdatabase;
-use todoappdatabase;
--- Create the tables
-create table usuario(
-	id_usuario int primary key not null auto_increment,
-    nombres varchar(20) not null,
-    apellidos varchar(20) not null,
-    correo varchar(100) not null,
-    contrasena varchar(255) not null,
-    fecha_registro timestamp default current_timestamp,
-    fecha_actualizacion timestamp default current_timestamp on update current_timestamp
-);
-create table tarea(
-	id_tarea int primary key not null auto_increment,
-    id_usuario int not null,
-    titulo varchar(30),
-    descripcion varchar(100),
-    fecha timestamp,
-	FOREIGN KEY(id_usuario) REFERENCES usuario (id_usuario)
-	ON DELETE CASCADE 
-	ON UPDATE CASCADE
-);
+-- TODOAPP full script database (tables, data and store procedures)
+
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+--
+-- Host: localhost    Database: todoappdatabase
+-- ------------------------------------------------------
+-- Server version	8.0.36-0ubuntu0.22.04.1
+
+create database if not exists todoappdb;
+use todoappdb;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id_user` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `register_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `email_2` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (0000000001,'Marin','Kitagawa','marinadmin@email.com','$2y$11$7muMDOc9T.o4IeJaGxX5Yez5UFB9M3qDuYFVokvlao0GuKYf7U4Mi','2024-05-06 00:47:08','2024-05-10 01:43:17'),(0000000002,'Hayase','Nagatoro','nagakouhai@email.com','$2y$11$erW0OrMFOQrQ9BVKS4q3ye.q5403fIKBlksB8jbOAgqllZL8QvTgm','2024-05-13 19:36:41','2024-05-13 19:36:41'),(0000000003,'Misato','Katsuragi','misatokatsu@email.com','$2y$11$OUWJBnyIOcgg0247/uMrGOTNgeYwaELkqT7rfyMoEvmuQkM4VusKi','2024-05-14 22:15:44','2024-05-14 22:15:44'),(0000000004,'Airi','Gotou','GotouAiri@email.com','$2y$11$1AKPh1AMdTyR8n82LJkk/OCkobnTg4zF.wfmOVpz6LA7qm1XLzmM6','2024-05-14 22:44:36','2024-05-14 22:44:36'),(0000000005,'sadsadsa','asdsadasd','asdsadasd@email.com','$2y$11$NM3Ck9BcMt1A4yNW1OFkKeOgv16DHPxOAGZ7vl9jo8qdK7FVM86wq','2024-05-16 02:12:18','2024-05-16 02:12:18');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-16 15:23:27
 
 
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+--
+-- Host: localhost    Database: todoappdatabase
+-- ------------------------------------------------------
+-- Server version	8.0.36-0ubuntu0.22.04.1
 
--- Actualización de la estructura y ahora pasa a ser en ingles
-create table user(
-	id_user int zerofill primary key auto_increment,
-    name varchar(20) not null,
-    last_name varchar(20) not null,
-    email varchar(100) not null,
-    password varchar(255) not null,
-    register_date timestamp not null DEFAULT CURRENT_TIMESTAMP,
-    update_date timestamp not null DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `tasks`
+--
 
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tasks` (
+  `id_task` binary(16) NOT NULL,
+  `id_user` int unsigned NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_task`),
+  KEY `fk_id_user` (`id_user`),
+  CONSTRAINT `fk_id_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `tasks`
+--
 
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+INSERT INTO `tasks` VALUES (_binary 'º6\ïo\0%«s0T',2,'Tarea de prueba','Tarea de prueba','2024-05-14 21:07:45','2024-05-14 21:07:45'),(_binary '^\Ä\ï¿\ì\0%«s0T',4,'Cenar con Yoshida','Cenar con Yoshida en la noche luego del trabajo','2024-05-16 00:12:57','2024-05-16 01:57:57'),(_binary '\r\Ë#j\ï\Î\0%«s0T',2,'Practicar judo','DespuÃ©s de clases, ir a la clase de Judo a las 4pm','2024-05-13 20:47:46','2024-05-13 20:47:46'),(_binary '91û¿ª\ï$\0%«s0T',1,'Aprender JavaScript','Repasar y reforzar uso de JavaScript','2024-05-12 21:54:36','2024-05-13 01:00:47'),(_binary 'A\ãJa\ï\Î\0%«s0T',2,'Molestar a senpai otra vez','Molestar a senpai otra vez','2024-05-13 19:44:48','2024-05-13 20:47:15'),(_binary '³J\Ë\Ç\ï$\0%«s0T',1,'Aprender CSS','Repasar css','2024-05-13 01:24:43','2024-05-13 01:24:43'),(_binary '©±\â:j\ï\Î\0%«s0T',2,'Ir a la pijamada maÃ±ana','Ir a la pijamada de mis amigas en casa de Yoshi','2024-05-13 20:52:08','2024-05-14 21:07:26'),(_binary 'Á¶\Ç\ï$\0%«s0T',1,'Aprender Laravel','Vamos para Laravel!','2024-05-13 01:26:00','2024-05-13 01:26:00'),(_binary '\æ\Ôysx\ï\0%«s0T',1,'Programar en PHP','Realizar mis proyectos pendientes de PHP','2024-05-10 02:56:30','2024-05-13 01:27:38'),(_binary 'üœ1\ïo\0%«s0T',1,'Mejorar el sitio','Sigo mejorando el sitio','2024-05-14 20:38:57','2024-05-14 20:38:57');
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- Dump completed on 2024-05-16 15:23:28
+
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+--
+-- Host: localhost    Database: todoappdatabase
+-- ------------------------------------------------------
+-- Server version	8.0.36-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping routines for database 'todoappdatabase'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `deleteTask` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteTask`(in id_taskParam varchar(36))
+begin
+	DECLARE binary_uuid BINARY(16);
+    -- Convertir el UUID de entrada a su representación binaria
+    SET binary_uuid = UNHEX(REPLACE(id_taskParam, '-', ''));
+    delete from tasks where id_task = binary_uuid;
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `getInfoTaskById` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getInfoTaskById`(in id_taskParam varchar(36))
+begin
+	DECLARE binary_uuid BINARY(16);
+    -- Convertir el UUID de entrada a su representación binaria
+    SET binary_uuid = UNHEX(REPLACE(id_taskParam, '-', ''));
+    -- Realizar la comparación con el UUID almacenado en la tabla
+    SELECT bin_to_uuid(T.id_task) as id_task, title, description, updated_at
+    FROM tasks T
+    WHERE T.id_task = binary_uuid;
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `insertTask` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insertTask`(in id_userParam int, in titleParam varchar(30), in descriptionParam varchar (200))
+begin
+    insert into tasks(id_task, id_user, title, description) 
+	values(uuid_to_bin(uuid()), id_userParam, titleParam , descriptionParam);
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `tasksForUser` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `tasksForUser`(in id_userParam int)
+begin
+select bin_to_uuid(T.id_task) as id_task, T.title, T.description 
+from users U
+inner join tasks T
+on U.id_user = T.id_user
+where id_userParam = U.id_user
+order by T.updated_at desc;
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `updateTask` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `updateTask`(in id_taskParam varchar(36), in titleParam varchar(30), in descriptionParam varchar (200))
+begin
+	DECLARE binary_uuid BINARY(16);
+    -- Convertir el UUID de entrada a su representación binaria
+    SET binary_uuid = UNHEX(REPLACE(id_taskParam, '-', ''));
+	update tasks set title = titleParam, description = descriptionParam where binary_uuid = id_task;
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-16 15:23:28
