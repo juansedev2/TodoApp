378173e612c89502c9f8e180ed5c9c7a238a96d6
Jdev2 <jsab62tol@gmail.com>
2024-05-20 17:45:52 -0500

    Add the logger object on the app

--- src/core/Bootstrap.php
+++ src/core/Bootstrap.php
@@ -3,6 +3,7 @@
 use Jdev2\TodoApp\core\Injector;
 use Jdev2\TodoApp\core\database\DBConnection;
 use Jdev2\TodoApp\core\database\QueryBuilder;
+use Jdev2\TodoApp\core\utils\AppLogger;
 
 $APP_PATH = $_SERVER["DOCUMENT_ROOT"];
 Injector::set($APP_PATH, "app-route");
@@ -13,8 +14,17 @@ if(!Injector::get("config")["production"]){
     ini_set("display_errors", 1);
     ini_set("display_startup_errors", 1);
     error_reporting(E_ALL);
+}else{
+    error_reporting(0);
+    ini_set("display_errors", 0);
+    ini_set("display_startup_errors", 0);
 }
+
 date_default_timezone_set((require __DIR__ ."/config/Config.php")["time-zone"]); // Updathe the timezone to the app of the server
-//$pdo = DBConnection::ConnectDB(Injector::get("config")["database"]);
 Injector::set(new QueryBuilder(DBConnection::ConnectDB(Injector::get("config")["database"])), "querybuilder");
-//return new QueryBuilder($pdo);
+
+// Create the AppLogger and inject to depedences handler, also insert the first necessarylog
+$appLoger = new AppLogger($APP_PATH . "/logs/");
+$appLoger->addClientConnectionLog();
+Injector::set($appLoger, "logger");
+
